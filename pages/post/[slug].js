7714ced6dacf02008aab3accc68e67783d2f4d6a
pramodtng <psingay96@gmail.com>
2022-10-24 15:17:19 +0600

    changes

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,28 +1,32 @@
 import React from 'react'
-import { PostWidget, Categories, Author, BlogDetails, Comment, CommentForm } from '../../components';
+import { PostWidget, Categories, Author, BlogDetails, Comment, CommentForm, Loader } from '../../components';
 import { getPostDetails, getPosts } from './../../services/index';
+import { useRouter } from 'next/router';
 
 
 
 const Blogs = ({ post }) => {
-  console.log(post.slug)
+  const router = useRouter()
+  if(router.isFallback) {
+    return <Loader />
+  }
   return (
-      <div className='container mx-auto px-10 pb-8'>
-        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-          <div className='lg:col-span-8 col-span-1 text-white'>
-            <BlogDetails post={post} />
-            <Author author={post.author} />
-            <CommentForm slug={post.slug} />
-            <Comment slug={post.slug} />
-          </div>
-          <div className='col-span-1 lg:col-span-4'>
-            <div className='relative lg:sticky top-8'>
-              <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
-              <Categories />
-            </div>
+    <div className='container mx-auto px-10 pb-8'>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1 text-white'>
+          <BlogDetails post={post} />
+          <Author author={post.author} />
+          <CommentForm slug={post.slug} />
+          <Comment slug={post.slug} />
+        </div>
+        <div className='col-span-1 lg:col-span-4'>
+          <div className='relative lg:sticky top-8'>
+            <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
+            <Categories />
           </div>
         </div>
       </div>
+    </div>
   )
 }
 export async function getStaticProps({ params }) {
